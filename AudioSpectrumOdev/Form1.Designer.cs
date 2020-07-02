648a11fbd7c6b76faff61344b6f1c1bd021e11f5
Fuatcan Genc <fuatcangenc@gmail.com>
2020-07-02 22:58:03 +0300

    Audio Spectrum

---
+++ AudioSpectrumOdev/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace rawshed
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bar16 = new VerticalProgressBar();
+            this.bar15 = new VerticalProgressBar();
+            this.bar14 = new VerticalProgressBar();
+            this.bar13 = new VerticalProgressBar();
+            this.bar12 = new VerticalProgressBar();
+            this.bar11 = new VerticalProgressBar();
+            this.bar10 = new VerticalProgressBar();
+            this.bar9 = new VerticalProgressBar();
+            this.bar8 = new VerticalProgressBar();
+            this.bar7 = new VerticalProgressBar();
+            this.bar6 = new VerticalProgressBar();
+            this.bar5 = new VerticalProgressBar();
+            this.bar4 = new VerticalProgressBar();
+            this.bar3 = new VerticalProgressBar();
+            this.bar2 = new VerticalProgressBar();
+            this.bar1 = new VerticalProgressBar();
+            this.devicelist = new System.Windows.Forms.ComboBox();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.bar16);
+            this.panel1.Controls.Add(this.bar15);
+            this.panel1.Controls.Add(this.bar14);
+            this.panel1.Controls.Add(this.bar13);
+            this.panel1.Controls.Add(this.bar12);
+            this.panel1.Controls.Add(this.bar11);
+            this.panel1.Controls.Add(this.bar10);
+            this.panel1.Controls.Add(this.bar9);
+            this.panel1.Controls.Add(this.bar8);
+            this.panel1.Controls.Add(this.bar7);
+            this.panel1.Controls.Add(this.bar6);
+            this.panel1.Controls.Add(this.bar5);
+            this.panel1.Controls.Add(this.bar4);
+            this.panel1.Controls.Add(this.bar3);
+            this.panel1.Controls.Add(this.bar2);
+            this.panel1.Controls.Add(this.bar1);
+            this.panel1.Location = new System.Drawing.Point(57, 81);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(293, 166);
+            this.panel1.TabIndex = 0;
+            // 
+            // bar16
+            // 
+            this.bar16.Location = new System.Drawing.Point(272, 2);
+            this.bar16.Margin = new System.Windows.Forms.Padding(2);
+            this.bar16.Maximum = 255;
+            this.bar16.Name = "bar16";
+            this.bar16.Size = new System.Drawing.Size(15, 162);
+            this.bar16.TabIndex = 15;
+            // 
+            // bar15
+            // 
+            this.bar15.Location = new System.Drawing.Point(254, 2);
+            this.bar15.Margin = new System.Windows.Forms.Padding(2);
+            this.bar15.Maximum = 255;
+            this.bar15.Name = "bar15";
+            this.bar15.Size = new System.Drawing.Size(15, 162);
+            this.bar15.TabIndex = 14;
+            // 
+            // bar14
+            // 
+            this.bar14.Location = new System.Drawing.Point(236, 2);
+            this.bar14.Margin = new System.Windows.Forms.Padding(2);
+            this.bar14.Maximum = 255;
+            this.bar14.Name = "bar14";
+            this.bar14.Size = new System.Drawing.Size(15, 162);
+            this.bar14.TabIndex = 13;
+            // 
+            // bar13
+            // 
+            this.bar13.Location = new System.Drawing.Point(218, 2);
+            this.bar13.Margin = new System.Windows.Forms.Padding(2);
+            this.bar13.Maximum = 255;
+            this.bar13.Name = "bar13";
+            this.bar13.Size = new System.Drawing.Size(15, 162);
+            this.bar13.TabIndex = 12;
+            // 
+            // bar12
+            // 
+            this.bar12.Location = new System.Drawing.Point(200, 2);
+            this.bar12.Margin = new System.Windows.Forms.Padding(2);
+            this.bar12.Maximum = 255;
+            this.bar12.Name = "bar12";
+            this.bar12.Size = new System.Drawing.Size(15, 162);
+            this.bar12.TabIndex = 11;
+            // 
+            // bar11
+            // 
+            this.bar11.Location = new System.Drawing.Point(182, 2);
+            this.bar11.Margin = new System.Windows.Forms.Padding(2);
+            this.bar11.Maximum = 255;
+            this.bar11.Name = "bar11";
+            this.bar11.Size = new System.Drawing.Size(15, 162);
+            this.bar11.TabIndex = 10;
+            // 
+            // bar10
+            // 
+            this.bar10.Location = new System.Drawing.Point(164, 2);
+            this.bar10.Margin = new System.Windows.Forms.Padding(2);
+            this.bar10.Maximum = 255;
+            this.bar10.Name = "bar10";
+            this.bar10.Size = new System.Drawing.Size(15, 162);
+            this.bar10.TabIndex = 9;
+            // 
+            // bar9
+            // 
+            this.bar9.Location = new System.Drawing.Point(146, 2);
+            this.bar9.Margin = new System.Windows.Forms.Padding(2);
+            this.bar9.Maximum = 255;
+            this.bar9.Name = "bar9";
+            this.bar9.Size = new System.Drawing.Size(15, 162);
+            this.bar9.TabIndex = 8;
+            // 
+            // bar8
+            // 
+            this.bar8.Location = new System.Drawing.Point(128, 2);
+            this.bar8.Margin = new System.Windows.Forms.Padding(2);
+            this.bar8.Maximum = 255;
+            this.bar8.Name = "bar8";
+            this.bar8.Size = new System.Drawing.Size(15, 162);
+            this.bar8.TabIndex = 7;
+            // 
+            // bar7
+            // 
+            this.bar7.Location = new System.Drawing.Point(110, 2);
+            this.bar7.Margin = new System.Windows.Forms.Padding(2);
+            this.bar7.Maximum = 255;
+            this.bar7.Name = "bar7";
+            this.bar7.Size = new System.Drawing.Size(15, 162);
+            this.bar7.TabIndex = 6;
+            // 
+            // bar6
+            // 
+            this.bar6.Location = new System.Drawing.Point(92, 2);
+            this.bar6.Margin = new System.Windows.Forms.Padding(2);
+            this.bar6.Maximum = 255;
+            this.bar6.Name = "bar6";
+            this.bar6.Size = new System.Drawing.Size(15, 162);
+            this.bar6.TabIndex = 5;
+            // 
+            // bar5
+            // 
+            this.bar5.Location = new System.Drawing.Point(74, 2);
+            this.bar5.Margin = new System.Windows.Forms.Padding(2);
+            this.bar5.Maximum = 255;
+            this.bar5.Name = "bar5";
+            this.bar5.Size = new System.Drawing.Size(15, 162);
+            this.bar5.TabIndex = 4;
+            // 
+            // bar4
+            // 
+            this.bar4.Location = new System.Drawing.Point(56, 2);
+            this.bar4.Margin = new System.Windows.Forms.Padding(2);
+            this.bar4.Maximum = 255;
+            this.bar4.Name = "bar4";
+            this.bar4.Size = new System.Drawing.Size(15, 162);
+            this.bar4.TabIndex = 3;
+            // 
+            // bar3
+            // 
+            this.bar3.Location = new System.Drawing.Point(38, 2);
+            this.bar3.Margin = new System.Windows.Forms.Padding(2);
+            this.bar3.Maximum = 255;
+            this.bar3.Name = "bar3";
+            this.bar3.Size = new System.Drawing.Size(15, 162);
+            this.bar3.TabIndex = 2;
+            // 
+            // bar2
+            // 
+            this.bar2.Location = new System.Drawing.Point(20, 2);
+            this.bar2.Margin = new System.Windows.Forms.Padding(2);
+            this.bar2.Maximum = 255;
+            this.bar2.Name = "bar2";
+            this.bar2.Size = new System.Drawing.Size(15, 162);
+            this.bar2.TabIndex = 1;
+            // 
+            // bar1
+            // 
+            this.bar1.Location = new System.Drawing.Point(2, 2);
+            this.bar1.Margin = new System.Windows.Forms.Padding(2);
+            this.bar1.Maximum = 255;
+            this.bar1.Name = "bar1";
+            this.bar1.Size = new System.Drawing.Size(15, 162);
+            this.bar1.TabIndex = 0;
+            // 
+            // devicelist
+            // 
+            this.devicelist.FormattingEnabled = true;
+            this.devicelist.Location = new System.Drawing.Point(4, 17);
+            this.devicelist.Margin = new System.Windows.Forms.Padding(2);
+            this.devicelist.Name = "devicelist";
+            this.devicelist.Size = new System.Drawing.Size(156, 21);
+            this.devicelist.TabIndex = 2;
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.BackColor = System.Drawing.Color.Orange;
+            this.btnEnable.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEnable.Location = new System.Drawing.Point(377, 132);
+            this.btnEnable.Margin = new System.Windows.Forms.Padding(0);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(155, 31);
+            this.btnEnable.TabIndex = 16;
+            this.btnEnable.Text = "Başlat";
+            this.btnEnable.UseVisualStyleBackColor = false;
+            this.btnEnable.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Location = new System.Drawing.Point(377, 168);
+            this.btnDisable.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(155, 31);
+            this.btnDisable.TabIndex = 18;
+            this.btnDisable.Text = "Durdur";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.BtnDisable_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.devicelist);
+            this.groupBox1.Location = new System.Drawing.Point(372, 83);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(166, 45);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dinlenecek Aygıt";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(221, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 16);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Audio Spectrum Ödevi";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(-4, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(606, 35);
+            this.label2.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(596, 350);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Audio Spectrum";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private VerticalProgressBar bar1;
+		private VerticalProgressBar bar16;
+		private VerticalProgressBar bar15;
+		private VerticalProgressBar bar14;
+		private VerticalProgressBar bar13;
+		private VerticalProgressBar bar12;
+		private VerticalProgressBar bar11;
+		private VerticalProgressBar bar10;
+		private VerticalProgressBar bar9;
+		private VerticalProgressBar bar8;
+		private VerticalProgressBar bar7;
+		private VerticalProgressBar bar6;
+		private VerticalProgressBar bar5;
+		private VerticalProgressBar bar4;
+		private VerticalProgressBar bar3;
+		private VerticalProgressBar bar2;
+		private System.Windows.Forms.ComboBox devicelist;
+		private System.Windows.Forms.Button btnEnable;
+		private System.Windows.Forms.Button btnDisable;
+		private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AudioSpectrumOdev/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Un4seen.Bass;
+using Un4seen.BassWasapi;
+
+
+namespace rawshed
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private bool _enable;               //calisma durumu
+		private DispatcherTimer _t;         //yenilemek icin timer
+		private float[] _fft;               //hizli fourier donusumu icin buffer
+		//private ProgressBar _l, _r;         //progressbars for left and right channel intensity
+		private WASAPIPROC _process;        
+		private int _lastlevel;             //son cikis seviyesi
+		private int _hanctr;                //son cikis seviyesi sayaci
+		private List<byte> _spectrumdata;   //spectrum veri bufferi
+		//private Spectrum _spectrum;         //spectrum dispay control
+		private ComboBox _devicelist;       //aygit listesi
+		private bool _initialized;          //basladi mi
+		private int devindex;               //kullanilan aygit
+
+		private int _lines = 16;            // kac spectrum var
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			btnDisable.Enabled = false;
+			_fft = new float[1024];
+			_lastlevel = 0;
+			_hanctr = 0;
+			_t = new DispatcherTimer();
+			_t.Tick += _t_Tick;
+			_t.Interval = TimeSpan.FromMilliseconds(25); //40hz yenileme hizi
+			_t.IsEnabled = false;
+			_process = new WASAPIPROC(Process);
+			_spectrumdata = new List<byte>();
+			_devicelist = devicelist;
+			_initialized = false;
+			Init();
+
+		}
+
+		public void setSpectrum(List<byte> data)
+		{
+			//MessageBox.Show("DataCount: " + data.Count);
+			if (data.Count < 16) return;
+			//MessageBox.Show(data[0].ToString());
+			bar1.Value = data[0];
+			bar2.Value = data[1];
+			bar3.Value = data[2];
+			bar4.Value = data[3];
+			bar5.Value = data[4];
+			bar6.Value = data[5];
+			bar7.Value = data[6];
+			bar8.Value = data[7];
+			bar9.Value = data[8];
+			bar10.Value = data[9];
+			bar11.Value = data[10];
+			bar12.Value = data[11];
+			bar13.Value = data[12];
+			bar14.Value = data[13];
+			bar15.Value = data[14];
+			bar16.Value = data[15];
+		}
+		//programi aktif ve deaktif etmek icin flag ayaralayalim
+		public bool Enable
+		{
+			get { return _enable; }
+			set
+			{
+				_enable = value;
+				if (value)
+				{
+					if (!_initialized)
+					{
+						var str = (_devicelist.Items[_devicelist.SelectedIndex] as string);
+
+						var array = str.Split(' ');
+						devindex = Convert.ToInt32(array[0]);
+						bool result = BassWasapi.BASS_WASAPI_Init(devindex, 0, 0,
+																  BASSWASAPIInit.BASS_WASAPI_BUFFER,
+																  1f, 0.05f,
+																  _process, IntPtr.Zero);
+						if (!result)
+						{
+							var error = Bass.BASS_ErrorGetCode();
+							MessageBox.Show(error.ToString());
+						}
+						else
+						{
+							_initialized = true;
+							_devicelist.Enabled = false;
+						}
+					}
+					BassWasapi.BASS_WASAPI_Start();
+				}
+				else BassWasapi.BASS_WASAPI_Stop(true);
+				System.Threading.Thread.Sleep(500);
+				_t.IsEnabled = value;
+			}
+		}
+		// programi ve kutuphanemizi baslatalim
+		private void Init()
+		{
+			bool result = false;
+			for (int i = 0; i < BassWasapi.BASS_WASAPI_GetDeviceCount(); i++)
+			{
+				var device = BassWasapi.BASS_WASAPI_GetDeviceInfo(i);
+				if (device.IsEnabled && device.IsLoopback)
+				{
+					_devicelist.Items.Add(string.Format("{0} - {1}", i, device.name));
+				}
+			}
+			_devicelist.SelectedIndex = 0;
+			Bass.BASS_SetConfig(BASSConfig.BASS_CONFIG_UPDATETHREADS, false);
+			result = Bass.BASS_Init(0, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+			if (!result) throw new Exception("Init Error");
+		}
+
+		//timer 
+		private void _t_Tick(object sender, EventArgs e)
+		{
+			// fft alalim (fast fourier transform). hata varsa -1 dondurur.
+			int ret = BassWasapi.BASS_WASAPI_GetData(_fft, (int)BASSData.BASS_DATA_FFT2048);
+			if (ret < 0) return;
+			int x, y;
+			int b0 = 0;
+
+			//spectrumun datasini hesaplayalim.
+			for (x = 0; x < _lines; x++)
+			{
+				float peak = 0;
+				int b1 = (int)Math.Pow(2, x * 10.0 / (_lines - 1));
+				if (b1 > 1023) b1 = 1023;
+				if (b1 <= b0) b1 = b0 + 1;
+				for (; b0 < b1; b0++)
+				{
+					if (peak < _fft[1 + b0]) peak = _fft[1 + b0];
+				}
+				y = (int)(Math.Sqrt(peak) * 3 * 255 - 4);
+				if (y > 255) y = 255;
+				if (y < 0) y = 0;
+				_spectrumdata.Add((byte)y);
+			}
+
+			if (_enable) setSpectrum(_spectrumdata);
+			_spectrumdata.Clear();
+
+
+			int level = BassWasapi.BASS_WASAPI_GetLevel();
+			if (level == _lastlevel && level != 0) _hanctr++;
+			_lastlevel = level;
+
+			//glitch olmamasi icin belli bir asima ugradiysa yeniden baslatalim
+			if (_hanctr > 3)
+			{
+				_hanctr = 0;
+				Free();
+				Bass.BASS_Init(0, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+				_initialized = false;
+				Enable = true;
+			}
+		}
+
+		// surekli calismasi icin gerekli
+		private int Process(IntPtr buffer, int length, IntPtr user)
+		{
+			return length;
+		}
+
+		private void Button1_Click(object sender, EventArgs e)
+		{
+			Enable = true;
+			btnEnable.Enabled = false;
+			btnDisable.Enabled = true;
+
+		}
+
+		private void BtnDisable_Click(object sender, EventArgs e)
+		{
+			Enable = false;
+			btnDisable.Enabled = false;
+			btnEnable.Enabled = true;
+			List<byte> _cop = new List<byte>(new byte[16]);
+			setSpectrum(_cop);
+		}
+
+		//temizlik
+		public void Free()
+		{
+			BassWasapi.BASS_WASAPI_Free();
+			Bass.BASS_Free();
+		}
+	}
+}
